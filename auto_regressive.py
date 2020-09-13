8d350ee63816e195c856bc2bf2f5c724cd227aaf
Lara Rakocevic <lrakocev@mit.edu>
2020-09-13 11:46:53 +0200

    top p

--- auto_regressive.py
+++ auto_regressive.py
@@ -106,7 +106,7 @@ def auto_regressive(model_info, curr_context, num_return_seqs, current_len, max_
 def auto_regressive_top_p(model_info, curr_context, num_return_seqs, current_len, max_len, total_js, joint_vocab, top_p):
 
     if current_len == max_len:
-        return total_js / current_len
+        return total_js / len(curr_context.split(" "))
 
     distrs = {}
     highest = {}
@@ -120,13 +120,13 @@ def auto_regressive_top_p(model_info, curr_context, num_return_seqs, current_len
     # average the two distributions
     avg_distr = {x: (A.get(x, 0) + B.get(x, 0))/2 for x in set(A).intersection(B)}
 
-    avg_distr = sorted(avg_distr.items(), key=lambda x: x[1], reverse=True)
+    avg_distr_sorted = sorted(avg_distr.items(), key=lambda x: x[1], reverse=True)
 
-    avg_distr_vals = [sum(avg_distr[:i+1]) for i in range(len(avg_distr))]
+    avg_distr_vals = [sum(avg_distr_sorted[:i+1][1]) for i in range(len(avg_distr_sorted))]
 
     avg_distr_summed = zip(avg_distr.keys(), avg_distr_vals)
 
-    avg_distr = {k: v for (k, v) in avg_distr_summed.keys() if tot_sum <= top_p}
+    avg_distr = {k: avg_distr[k] for (k, v) in avg_distr_summed.keys() if v <= top_p}
 
     prob_list = [v for k, v in sorted(avg_distr.items())]
     word_list = [k for k, v in sorted(avg_distr.items())]
