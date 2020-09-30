4c45083edb680b7adb096db353c314c96e268c84
Lara Rakocevic <lrakocev@mit.edu>
2020-09-30 11:13:14 +0200

    indexing

--- judge_sentences.py
+++ judge_sentences.py
@@ -16,7 +16,7 @@ def get_distribution(model_info, model_name, context, joint_vocab):
 
   model, tokenizer = model_info[model_name]
 
-  input = tokenizer(context,return_tensors='tf',pad_to_max_length=True)
+  input = tokenizer(context,return_tensors='tf',pad_to_max_length=True,padding_value=tokenizer.eos_token_id)
   outputs = model(input)
 
   ids = range(0,tokenizer.vocab_size)
