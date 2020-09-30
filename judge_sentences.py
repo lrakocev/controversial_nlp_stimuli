88ffefee0cfb8384725828598662491579946ddd
Lara Rakocevic <lrakocev@mit.edu>
2020-09-30 13:52:19 +0200

    spaces

--- judge_sentences.py
+++ judge_sentences.py
@@ -252,8 +252,6 @@ joint_vocab = gpt2_dict.keys() & txl_dict.keys()
 
 #for i in range(5):
 
-' '.join(sample_sentences("sentences4lara.txt").split())
-
 sent = ' '.join(sample_sentences("sentences4lara.txt").split())
 scores, js_positions, sentence = change_sentence(model_info, sent, joint_vocab, 5, .9)
 plot_scores(scores, sentence)
