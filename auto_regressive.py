fa0153605789e4517af66d5c34bb38ccb3b143bf
Lara Rakocevic <lrakocev@mit.edu>
2020-10-05 13:27:31 +0200

    adding models

--- auto_regressive.py
+++ auto_regressive.py
@@ -188,12 +188,12 @@ def auto_regressive_top_p(model_info, curr_context, num_return_seqs, current_len
     # then autoregressive again on this new current context
     return auto_regressive_top_p(model_info, curr_context, num_return_seqs, current_len + 1, max_len , total_js, joint_vocab, top_p)
 
-model_info = {"GPT2": (AutoModelWithLMHead.from_pretrained('gpt2-large',from_tf=True), AutoTokenizer.from_pretrained('gpt2-large')), 
+model_info = {"GPT2": (AutoModelWithLMHead.from_pretrained('gpt2-large'), AutoTokenizer.from_pretrained('gpt2-large')), 
               "TransformerXL": (TFTransfoXLLMHeadModel.from_pretrained('transfo-xl-wt103'),TransfoXLTokenizer.from_pretrained('transfo-xl-wt103')),
-              "T5": (AutoModelWithLMHead.from_pretrained("t5-11b",from_tf=True), AutoTokenizer.from_pretrained("t5-11b")),
-              "Roberta": (TFRobertaModel.from_pretrained('roberta-base'),RobertaTokenizer.from_pretrained('roberta-base')),
-              "Albert": (TFAlbertModel.from_pretrained('albert-base-v2'), AlbertTokenizer.from_pretrained('albert-base-v2')),
-              "XLM": ( TFXLMModel.from_pretrained('xlm-mlm-xnli15-1024'), XLMTokenizer.from_pretrained('xlm-mlm-xnli15-1024'))}
+              "T5": (AutoModelWithLMHead.from_pretrained("t5-11b"), AutoTokenizer.from_pretrained("t5-11b")),
+              "Roberta": (RobertaModel.from_pretrained('roberta-base'),RobertaTokenizer.from_pretrained('roberta-base')),
+              "Albert": (AlbertModel.from_pretrained('albert-base-v2'), AlbertTokenizer.from_pretrained('albert-base-v2')),
+              "XLM": ( XLMModel.from_pretrained('xlm-mlm-xnli15-1024'), XLMTokenizer.from_pretrained('xlm-mlm-xnli15-1024'))}
 
 curr_context = sys.argv[1:]
 curr_context = ' '.join(curr_context)
