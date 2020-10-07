50af4559ec12851d634bcca6a8f175d78dba11c7
Lara Rakocevic <lrakocev@mit.edu>
2020-10-07 12:11:55 +0200

    t5

--- judge_sentences.py
+++ judge_sentences.py
@@ -246,10 +246,14 @@ def sample_sentences(file_name):
 
   return " ".join(line)
 
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+cuda = torch.cuda.is_available()
+
+t5_config = T5Config.from_pretrained(T5_PATH, cache_dir='./pretrained_models')
 
 model_info = {"GPT2": (TFGPT2LMHeadModel.from_pretrained('gpt2'), GPT2Tokenizer.from_pretrained('gpt2')), 
               "TransformerXL": (TFTransfoXLLMHeadModel.from_pretrained('transfo-xl-wt103'),TransfoXLTokenizer.from_pretrained('transfo-xl-wt103')),
-              "T5": (AutoModel.from_pretrained("t5-11b",use_cdn = False), T5Tokenizer.from_pretrained("t5-11b")), 
+              "T5": (T5ForConditionalGeneration.from_pretrained("t5-11b", config=t5_config, cache_dir='./pretrained_models').to(DEVICE), T5Tokenizer.from_pretrained("t5-11b", cache_dir='./pretrained_models')), 
               "Roberta": (AutoModel.from_pretrained("roberta-base"),RobertaTokenizer.from_pretrained("roberta-base")),
               "Albert": (AlbertModel.from_pretrained('albert-base-v2'), AlbertTokenizer.from_pretrained('albert-base-v2')),
               "XLM": ( XLMModel.from_pretrained('xlm-mlm-xnli15-1024'), XLMTokenizer.from_pretrained('xlm-mlm-xnli15-1024'))}
