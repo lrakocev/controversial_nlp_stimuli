0c024bb1560909e0117ba0af0721f32cf1fec18a
Lara Rakocevic <lrakocev@mit.edu>
2020-10-08 13:16:55 +0200

    basic

--- judge_sentences.py
+++ judge_sentences.py
@@ -258,8 +258,10 @@ xlm_config = XLMConfig.from_pretrained('xlm-mlm-xnli15-1024', cache_dir='./pretr
 
 model_info = {"GPT2": (TFGPT2LMHeadModel.from_pretrained('gpt2'), GPT2Tokenizer.from_pretrained('gpt2')), 
               "TransformerXL": (TFTransfoXLLMHeadModel.from_pretrained('transfo-xl-wt103'),TransfoXLTokenizer.from_pretrained('transfo-xl-wt103')),
-              "T5": (T5ForConditionalGeneration.from_pretrained("t5-11b", config=t5_config, cache_dir='./pretrained_models',use_cdn = False).to(DEVICE), T5Tokenizer.from_pretrained("t5-11b", cache_dir='./pretrained_models')),
-              "Albert": (AlbertModel.from_pretrained("albert-base-v2",config=albert_config, cache_dir='./pretrained_models',use_cdn = False).to(DEVICE), AlbertTokenizer.from_pretrained('albert-base-v2')) }
+              "T5": (TFT5Model.from_pretrained('t5-small'), T5Tokenizer.from_pretrained('t5-small')),
+              "Roberta":(TFRobertaModel.from_pretrained('roberta-base'), RobertaTokenizer.from_pretrained('roberta-base')),
+              "XLM": (TFXLMModel.from_pretrained('xlm-mlm-en-2048'), XLMTokenizer.from_pretrained('xlm-mlm-en-2048')),
+              "Albert":(TFAlbertModel.from_pretrained('albert-base-v2'),AlbertTokenizer.from_pretrained('albert-base-v2'))}
 
 '''
 "Roberta": (RobertaModel.from_pretrained("roberta-base",config=roberta_config, cache_dir='./pretrained_models',use_cdn = False).to(DEVICE), RobertaTokenizer.from_pretrained("roberta-base")),
