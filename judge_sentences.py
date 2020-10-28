80462111b85213c4ebcdd0640c0d8fc88cd0119f
Lara Rakocevic <lrakocev@mit.edu>
2020-10-28 12:38:46 -0700

    weights

--- judge_sentences.py
+++ judge_sentences.py
@@ -105,10 +105,16 @@ def evaluate_sentence(model_list, sentence, vocab):
       tokenizer = model_name.tokenizer
       model = model_name.model
 
+
+
       next_word_distr = get_distribution(model_name, curr_context, sentence_split[i+1], vocab)
       distrs[model_name] = next_word_distr
 
-    total_js += jsd(distrs.values())
+
+    n = len(model_list)
+    weights = np.empty(n)
+    weights.fill(1/n)
+    total_js += jsd(distrs.values(), weights)
     curr_js = total_js/(i+1)
     js_positions.append(curr_js)
     
