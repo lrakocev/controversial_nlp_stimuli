6f7c92d4776883bdb32e62681d95a95c2f728f7c
Lara Rakocevic <lrakocev@mit.edu>
2020-10-28 18:57:29 -0700

    logic fix for multiple tokens

--- judge_sentences.py
+++ judge_sentences.py
@@ -79,8 +79,8 @@ def get_distribution(model_name, context, next_word, vocab):
       next_probabilties, next_distr_dict = get_distribution(model_name, new_context, next_word_tokens[i+1], vocab)
       probabilities += vectorize_log(next_probabilities)
 
-  distr_dict = dict(zip(vocab, tot_probabilities))
-  return tot_probabilities, distr_dict
+  distr_dict = dict(zip(vocab, probabilities))
+  return probabilities, distr_dict
 
 
 def jsd(prob_distributions, weights, logbase=math.e):
