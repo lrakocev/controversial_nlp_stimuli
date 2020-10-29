adfa43b250d1f37cc8926f189f20e146bc8ce463
Lara Rakocevic <lrakocev@mit.edu>
2020-10-28 22:28:25 -0700

    debug

--- judge_sentences.py
+++ judge_sentences.py
@@ -77,8 +77,8 @@ def get_distribution(model_name, context, next_word, vocab):
 
     print("output logits 0", outputs.logits[0])
 
-    print("range len", range(len(np.asarray(outputs.logits[i].detach()).flatten())) )
-    log_probabilities = [vectorize_log(np.asarray(outputs.logits.detach())[i].flatten()) for i in range(len(np.asarray(outputs.logits[i].detach()).flatten()))]
+    print("range len", range(len(np.asarray(outputs.logits.detach()).flatten())) )
+    log_probabilities = [vectorize_log(np.asarray(outputs.logits[i].detach()).flatten()) for i in range(len(np.asarray(outputs.logits.detach()).flatten()))]
     print("len probs, see if same as number tokens next", log_probabilities.size())
     summed_log_probs = np.sum(log_probabilities, axis=1)
     probailities = softmax(summed_log_probs)
