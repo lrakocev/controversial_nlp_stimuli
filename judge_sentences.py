7986bcba62c578523767f608cdf2cd6be6572da5
Lara Rakocevic <lrakocev@mit.edu>
2020-11-03 12:22:00 -0800

    debugging

--- judge_sentences.py
+++ judge_sentences.py
@@ -92,9 +92,7 @@ def get_distribution(model_name, context, vocab, n):
       batch = batch_list[i]
       if length < max_length: 
         added_string = " ".join([tokenizer.eos_token] * (max_length - length))
-        batch += added_string
-        print(batch)
-
+        batch = batch + " " + added_string
 
     inputs = tokenizer(batch_list, padding='longest', return_tensors="pt")
 
@@ -102,10 +100,18 @@ def get_distribution(model_name, context, vocab, n):
 
     vectorize_log = np.vectorize(math.log)
 
-    log_probabilities = [vectorize_log(softmax(np.asarray(outputs.logits[j][i].detach()).flatten())) for j in range(len(batch_list)) for i in range(max_length,0,-1)]
+    print("outputs logits", outputs.logits.size())
+
+    if 
+    log_probabilities = [vectorize_log(softmax(np.asarray(outputs.logits[j][i].detach()).flatten())) for j in range(len(batch_list)) for i in range(max_length,max_length - lengths_contexts[j],-1)]
+
+    print("log probabilities", len(log_probabilities))
 
     log_probabilities_per_tokens = [[log_probabilities[j][id_nums[j][i]] for i in range(len(id_nums[j]))] for j in range(len(batch_list))]
 
+
+
+    print(log_probabilities_per_tokens)
 
     probabilities = np.sum(log_probabilities_per_tokens, axis = 1)
 
