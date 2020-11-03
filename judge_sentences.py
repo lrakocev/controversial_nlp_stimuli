894597fa4f949e3f00a9e4b3258b45735e53ac7e
Lara Rakocevic <lrakocev@mit.edu>
2020-11-03 13:05:00 -0800

    debugging

--- judge_sentences.py
+++ judge_sentences.py
@@ -64,7 +64,7 @@ def get_distribution(model_name, context, vocab, n):
   model = model_name.model
   model_word_token_dict = model_name.word_token_dict
   model_token_id_dict = model_name.id_token_dict
-  tokenizer.pad_token = tokenizer.start_token_symbol
+  tokenizer.pad_token = model_name.start_token_symbol
 
   context_tokens = tokenizer.tokenize(context)
 
@@ -82,8 +82,6 @@ def get_distribution(model_name, context, vocab, n):
 
     max_length = max(lengths_contexts)
 
-    print("max length", max_length)
-
     id_nums = [[model_token_id_dict[token] for token in sub_word_tokens] for sub_word_tokens in sub_word_token_groupings ]
 
     for i in range(len(batch_list)):
@@ -99,12 +97,9 @@ def get_distribution(model_name, context, vocab, n):
 
     vectorize_log = np.vectorize(math.log)
 
-
     log_probabilities = [[vectorize_log(softmax(np.asarray(outputs.logits[j][i].detach()).flatten())) for i in range(max_length,max_length - lengths_contexts[j],-1)] for j in range(len(batch_list))]
 
     log_probabilities_per_tokens = [[log_probabilities[j][i][id_nums[j][i]] for i in range(len(id_nums[j]))] for j in range(len(batch_list))]
-
-    print(log_probabilities_per_tokens)
 
     probabilities = [sum(log_probabilities_per_tokens[i]) for i in range(len(log_probabilities_per_tokens))]
 
