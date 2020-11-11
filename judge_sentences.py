69427aa615bc72838c361ba8d79dba4699162d77
Lara Rakocevic <lrakocev@mit.edu>
2020-11-11 12:11:38 -0800

    trying diff models

--- judge_sentences.py
+++ judge_sentences.py
@@ -344,7 +344,7 @@ Albert = ModelInfo(AlbertForMaskedLM.from_pretrained('albert-base-v2', return_di
 
 
 #model_list = [GPT2, XLM]
-model_list = [TXL, Roberta]
+model_list = [TXL, XLM]
 
 for i in range(1):
 
