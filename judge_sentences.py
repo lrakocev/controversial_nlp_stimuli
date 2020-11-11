7c7004a25536135962f4bce12d23d6d4d134d060
Lara Rakocevic <lrakocev@mit.edu>
2020-11-11 12:48:23 -0800

    new models

--- judge_sentences.py
+++ judge_sentences.py
@@ -342,7 +342,7 @@ Albert = ModelInfo(AlbertForMaskedLM.from_pretrained('albert-base-v2', return_di
 
 
 #model_list = [GPT2, XLM]
-model_list = [GPT2, XLM, Roberta]
+model_list = [Roberta, Alberta, TXL]
 
 for i in range(1):
 
