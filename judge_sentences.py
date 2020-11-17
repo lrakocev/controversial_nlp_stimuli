40b06b79ae4929ffe53ffbd8c2b992e7a78d9906
Lara Rakocevic <lrakocev@mit.edu>
2020-11-16 18:36:51 -0800

    changes for 2 masks

--- judge_sentences.py
+++ judge_sentences.py
@@ -217,9 +217,7 @@ def get_avg_distr(model_list, context, vocab, n):
 def sample_bert(context, change_i, num_masks, top_k):
 
   context[change_i] = '[MASK]'
-  if num_masks == 2 and len(context) > change_i+1:
-    context[change_i+1] = '[MASK]'
-  elif num_masks == 2 and len(context) <= change_i+1:
+  if num_masks == 2:
     context.insert(change_i+1,'[MASK]')
 
   print("num masks", num_masks, 'bert context', context)
