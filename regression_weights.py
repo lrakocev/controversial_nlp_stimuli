331674150db16ceac0514af9922d86afe096e57f
Lara Rakocevic <lrakocev@mit.edu>
2020-11-19 18:40:04 -0800

    detach numpy

--- regression_weights.py
+++ regression_weights.py
@@ -39,6 +39,6 @@ for sent in sentences:
 
 	hiddenStates = outputs.hidden_states 
 	
-	hiddenStatesLayer = hiddenStates[-1]
+	hiddenStatesLayer = hiddenStates[-1].detach().numpy()
 	
 	new_model.predict(hiddenStatesLayer)
