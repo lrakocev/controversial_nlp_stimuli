0523de6f409104b276c03aee521ff73c40a1f76e
Lara Rakocevic <lrakocev@mit.edu>
2020-11-19 19:31:13 -0800

    bah

--- regression_weights.py
+++ regression_weights.py
@@ -13,7 +13,10 @@ score_name1 = '/om2/user/gretatu/.result_caching/neural_nlp.score/benchmark=Pere
 s = pd.read_pickle(score_name1)
 d = s['data']
 
-roberta_coeffs = d.layer_weights[0][0][-1].divider
+print(d.layer_weights[0][0][-1])
+
+
+roberta_coeffs = d.layer_weights[0][0][-1]
 
 print(roberta_coeffs)
 
