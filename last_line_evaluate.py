2dbe60a479e2dfd91476b157a600adb6ca03d3ab
Lara Rakocevic <lrakocev@mit.edu>
2020-11-21 10:37:40 -0800

    last line evaluate

--- last_line_evaluate.py
+++ last_line_evaluate.py
@@ -3,10 +3,12 @@ import os
 import glob
 file_list = glob.glob('evaluate*.out')
 
-score_dict = []
+score_dict = {}
 for file in file_list:
 	a_file = open(file, "r")
 	lines = a_file.readlines()
-	score_dict[lines[-2]] = lines[-1]
+	score = lines[-1]
+	sentence = lines[-2]
+	score_dict[sentence] = score
 
 print(score_dict)
