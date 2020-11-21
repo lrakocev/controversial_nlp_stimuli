2fccdecd91de13f97d44607189c69fbd5b7d6a40
Lara Rakocevic <lrakocev@mit.edu>
2020-11-21 11:58:19 -0800

    does this work

--- simple_jsd.py
+++ simple_jsd.py
@@ -38,7 +38,7 @@ def get_probabilities_alternative(model, tokenizer, sentence):
 	scores = []
 	for i in range(len(ids)):
 		ind = ids[i]
-		score = predictions[0][i][ind]
+		score = float(predictions[0][i][ind])
 		scores.append([score, 1-score])
 
 	return scores
