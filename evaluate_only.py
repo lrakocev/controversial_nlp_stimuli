49c4cc9ea7eeb1b46775fabe5663579669e414da
Lara Rakocevic <lrakocev@mit.edu>
2020-11-21 15:47:02 -0800

    reduce batch size

--- evaluate_only.py
+++ evaluate_only.py
@@ -6,7 +6,7 @@ filename = "SUBTLEXus74286wordstextversion.txt"
 vocab = j_s.get_vocab(filename, 10000)
 
 model_list = [j_s.GPT2, j_s.Roberta] 
-n = 100
+n = 10
 
 def evaluate_sentence(sentence, n):
 
