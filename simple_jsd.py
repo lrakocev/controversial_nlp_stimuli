d4703189c2e20404ed58aa69ed96a11807f06937
Lara Rakocevic <lrakocev@mit.edu>
2020-11-22 11:25:20 -0800

    okay

--- simple_jsd.py
+++ simple_jsd.py
@@ -1,4 +1,4 @@
-from transformers import pipeline, GPT2LMHeadModel, GPT2Tokenizer
+from transformers import pipeline, GPT2LMHeadModel, GPT2Tokenizer, RobertaTokenizer, RobertaForCausalLM
 from scipy.spatial import distance
 from scipy.special import softmax
 import numpy as np
@@ -29,17 +29,27 @@ def get_probabilities_alternative(model, tokenizer, sentence):
 	tokens = tokenizer.tokenize(sentence)
 	ids = tokenizer.convert_tokens_to_ids(tokens)
 
-	print(tokens)
+	sentence_split = sentence.split(" ")
+	word_to_ids = {}
+	for word in range(len(sentence_split)):
+		word_tokens = tokenizer.tokenize(word)
+		word_ids = tokenizer.convert_tokens_to_ids(word_tokens)
+		word_to_ids[word] = word_ids
+
 
 	inputs = tokenizer(sentence, return_tensors='pt')
 	outputs = model(**inputs)
 
-	predictions = nn.functional.softmax(outputs.logits, dim=2)
+	predictions = math.log(nn.functional.softmax(outputs.logits, dim=2))
 
 	scores = []
-	for i in range(len(ids)):
-		ind = ids[i]
-		score = float(predictions[0][i][ind])
+	for j in range(len(ids)):
+		word = sentence_split[j]
+		word_ids = word_to_ids[word]
+		score = 0
+		for i in word_ids:
+			ind = ids[i]
+			score += float(predictions[0][i][ind])
 		scores.append([score, 1-score])
 
 	return scores
@@ -68,7 +78,8 @@ nlp_roberta = pipeline("fill-mask", model="roberta-base")
 nlp_xlm = pipeline("fill-mask", model="xlm-mlm-xnli15-1024")
 GPT2_model = GPT2LMHeadModel.from_pretrained('gpt2', return_dict =True)
 GPT2_tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-
+Roberta_model = RobertaForCausalLM.from_pretrained('roberta-base',  return_dict=True)
+Roberta_tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
 
 final_jsd_scores = {}
 
@@ -76,6 +87,7 @@ for i in range(len(sentences)):
 	sentence = sentences[i]
 	#scores1 = get_probabilities(nlp_roberta, sentence)
 	#scores2 = get_probabilities(nlp_xlm, sentence)
+	scores1 = get_alternative_probabilities(Roberta_model, Roberta_tokenizer, sentence)
 	scores2 = get_probabilities_alternative(GPT2_model, GPT2_tokenizer, sentence)
 
 	#jsd = evaluate_sentence(scores1, scores2)
