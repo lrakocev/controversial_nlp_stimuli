7577d1a436f55d53d43a2393e0ec7b67cce9603a
Lara Rakocevic <lrakocev@mit.edu>
2020-11-22 11:54:55 -0800

    hmmm

--- simple_jsd.py
+++ simple_jsd.py
@@ -93,7 +93,7 @@ for i in range(len(sentences)):
 	scores1 = get_probabilities_alternative(Roberta_model, Roberta_tokenizer, sentence)
 	scores2 = get_probabilities_alternative(GPT2_model, GPT2_tokenizer, sentence)
 
-	#jsd = evaluate_sentence(scores1, scores2)
-	#final_jsd_scores[sentence] = jsd
+	jsd = evaluate_sentence(scores1, scores2)
+	final_jsd_scores[sentence] = jsd
 
-#print(final_jsd_scores)
+print(final_jsd_scores)
