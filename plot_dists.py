fea8637e2b769db1c38e4ddf468892301a5a6110
Lara Rakocevic <lrakocev@mit.edu>
2020-11-23 12:36:40 -0800

    V2 simple jsd vs cosine

--- plot_dists.py
+++ plot_dists.py
@@ -5,14 +5,14 @@ from scipy.stats import pearsonr
 import jsd_output
 import regression_weights as rw
 
-#simple_jsd_scores = simple_jsd.final_jsd_scores
+simple_jsd_scores = simple_jsd.final_jsd_scores
 simple_jsd_scores = jsd_output.js
 
 ordered_cosine_distances = rw.cosine_distances
 sentence_list = rw.sentence_list
 cosine_dict = dict(zip(sentence_list, ordered_cosine_distances))
 
-'''
+
 jsd_score_dict = lle.score_dict
 
 # removing the error lines
@@ -20,18 +20,28 @@ jsd_score_dict = {k:v for (k,v) in jsd_score_dict.items() if k in sentence_list}
 
 # getting rid of extra sentences
 cosine_dict = {k:v for (k,v) in cosine_dict.items() if k in jsd_score_dict.keys()}
-'''
+
 
 #getting vals in order of sorted keys
 cosine_scores = [float(v) for (k,v) in sorted(cosine_dict.items(), key=lambda x: x[0], reverse=True)]
-jsd_scores = [float(v) for (k,v) in sorted(simple_jsd_scores.items(), key=lambda x: x[0], reverse=True)]
+#jsd_scores = [float(v) for (k,v) in sorted(simple_jsd_scores.items(), key=lambda x: x[0], reverse=True)]
+jsd_scores = [float(v) for (k,v) in sorted(jsd_score_dict.items(), key=lambda x: x[0], reverse=True)]
 
-corr, _ = pearsonr(cosine_scores, jsd_scores)
 
-print("corr", corr)
+corr, p_value = pearsonr(cosine_scores, jsd_scores)
 
+lr_model = LinearRegression()
+
+slope, intercept = lr_model.fit(cosine_scores, jsd_scores)
+
+abline_values = [slope * i + intercept for i in cosine_scores]
+
+plt.plot(cosine_scores, abline_values, 'b')
 plt.scatter(cosine_scores, jsd_scores)
 plt.show()
 plt.savefig("V2 Simple JSD v Cosine")
+plt.xlabel("cosine distances")
+plt.ylabel("j-s divergence scores")
+plt.title("Pearson correlation score: " + corr + " and p-value: " + p_value)
 plt.close()
 
