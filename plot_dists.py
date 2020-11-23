12814be95ab6e208b72032c7061499865bb5bbcc
Lara Rakocevic <lrakocev@mit.edu>
2020-11-23 13:10:54 -0800

    plot dist

--- plot_dists.py
+++ plot_dists.py
@@ -3,6 +3,7 @@ import last_line_evaluate as lle
 from scipy.stats import pearsonr
 import regression_weights as rw
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
 '''
 import simple_jsd
 import jsd_output
@@ -28,21 +29,22 @@ cosine_scores = [float(v) for (k,v) in sorted(cosine_dict.items(), key=lambda x:
 #jsd_scores = [float(v) for (k,v) in sorted(simple_jsd_scores.items(), key=lambda x: x[0], reverse=True)]
 jsd_scores = [float(v) for (k,v) in sorted(jsd_score_dict.items(), key=lambda x: x[0], reverse=True)]
 
-
-corr, p_value = pearsonr(cosine_scores, jsd_scores)
-
 lr_model = LinearRegression()
 
-slope, intercept = lr_model.fit(cosine_scores, jsd_scores)
+lr_model.fit(cosine_scores, jsd_scores)
+slope = lr_model.coef_
+intercept = lr_model.intercept_
+
+r2= r2_score(cosine_scores, jsd_scores)
 
 abline_values = [slope * i + intercept for i in cosine_scores]
 
-plt.plot(cosine_scores, abline_values)
+plt.plot(cosine_scores, abline_values, color='black', linewidth=3)
 plt.scatter(cosine_scores, jsd_scores)
 plt.show()
 plt.savefig("V2 Simple JSD v Cosine")
 plt.xlabel("cosine distances")
 plt.ylabel("j-s divergence scores")
-plt.title("Pearson correlation score: " + corr + " and p-value: " + p_value)
+plt.title("R2: " + r2 + " and p-value: " + p_value)
 plt.close()
 
