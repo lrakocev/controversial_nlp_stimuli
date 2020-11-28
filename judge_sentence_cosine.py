36be86e01af0d1208e814f61baa29e77fc310543
Lara Rakocevic <lrakocev@mit.edu>
2020-11-28 11:05:28 -0800

    changes

--- judge_sentence_cosine.py
+++ judge_sentence_cosine.py
@@ -270,6 +270,9 @@ def plot_positions(js_positions, sentence):
     plt.plot(pos)
   ticks = sentence.split(" ")
   plt.xticks(np.arange(len(ticks)), ticks)
+  plt.xlabel("Iterations")
+  plt.ylabel("Cosine Distance Scores")
+  plt.title("GPT2-Roberta-Albert-XLM Cosine Distance")
   plt.show()
   name = sentence + " positions cosine.png"
   plt.savefig(name)
@@ -279,19 +282,19 @@ def change_sentence(model_list, sentence, vocab, batch_size, num_changes):
 
   scores = []
   changes = []
-  change = ""
   sentence_split = sentence.split(" ")
   len_sentence = len(sentence_split)
 
   curr_score = evaluate_sentence(model_list, ' '.join(sentence_split), vocab, batch_size)
-
+  changes.append((curr_score, ' '.join(sentence_split)))
   scores.append(curr_score)
 
   for change_i in range(0,num_changes):
 
     print("Curr sentence is: ", ' '.join(sentence_split), " with cosine distance: ", curr_score)
 
-    change_i = random.randint(0, len(sentence_split)-1)
+    # len-2 to avoid changing after ending punctuation
+    change_i = random.randint(0, len(sentence_split)-2)
 
     print("change i ", change_i)
 
@@ -300,41 +303,42 @@ def change_sentence(model_list, sentence, vocab, batch_size, num_changes):
     modified_sentence_additions = copy.copy(sentence_split)
 
     new_sentence_list = []
-    new_sentence_dict = {}
 
     # replacements 
     num_masks = random.randint(1,2)
 
-    new_word_list = sample_bert(sentence_split, change_i, num_masks, 10)
+    new_word_list = sample_bert(sentence_split, change_i, num_masks, 25)
 
+    i = 25
     for words in new_word_list: 
+      i -= 1
       if num_masks == 1:
          modified_sentence_replacements[change_i] = str(words)
-      if num_masks == 2 and len(modified_sentence_replacements) > change_i + 1:
-        modified_sentence_replacements[change_i] = str(words[0])
-        modified_sentence_replacements[change_i+1] = str(words[1])
-      elif num_masks == 2 and len(modified_sentence_replacements) <= change_i + 1:
+      #if num_masks == 2 and len(modified_sentence_replacements) > change_i + 1:
+      #  modified_sentence_replacements[change_i] = str(words[0])
+      #  modified_sentence_replacements[change_i+1] = str(words[1])
+      if num_masks == 2:
         modified_sentence_replacements[change_i] = str(words[0])
         modified_sentence_replacements.insert(change_i+1,str(words[1]))
 
       new_context = ' '.join(modified_sentence_replacements)
       print("mod sentence replacement", new_context)
-      new_sentence_list.append(new_context)
-      #new_sentence_dict[new_context] = evaluate_sentence(model_list, new_context, vocab, batch_size)
+      new_sentence_list.append((i, new_context))
       
     #deletions
     modified_sentence_deletions.pop(change_i)
     if len(modified_sentence_deletions) > 0:
       print("deletion try", ' '.join(modified_sentence_deletions))
       new_context = ' '.join(modified_sentence_deletions)
-      new_sentence_list.append(new_context)
-      #new_sentence_dict[new_context] = evaluate_sentence(model_list, new_context, vocab, batch_size)
-
+      new_sentence_list.append((1,new_context))
+      
     # additions
     num_masks = random.randint(1,2)
-    new_word_list = sample_bert(sentence_split, change_i, num_masks, 10)
+    new_word_list = sample_bert(sentence_split, change_i, num_masks, 25)
 
+    i = 25
     for words in new_word_list:
+      i -= 1
       print("words", words)
       if num_masks == 1:
         modified_sentence_additions.insert(change_i+1,str(words))
@@ -342,30 +346,32 @@ def change_sentence(model_list, sentence, vocab, batch_size, num_changes):
         modified_sentence_additions.insert(change_i+1,str(words[0]))
         modified_sentence_additions.insert(change_i+2,str(words[1]))
 
-
       new_context = ' '.join(modified_sentence_additions)
       print("mod sentence additions", new_context)
-      new_sentence_list.append(new_context)
-      #new_sentence_dict[new_context] = evaluate_sentence(model_list, new_context, vocab, batch_size)
+      new_sentence_list.append((i,new_context))
       modified_sentence_additions = copy.copy(sentence_split)
 
-    sampled_id = random.randint(0, len(new_sentence_list)-1)
+    #sampled_id = random.randint(0, len(new_sentence_list)-1)
+    exponentiated_scores = torch.tensor(softmax([i[0] for i in new_sentence_list]))
+    n = list(torch.multinomial(exponentiated_scores, 1))
+    sampled_id = n[0]
     final_modified_sentence = new_sentence_list[sampled_id]
     new_sentence_score = evaluate_sentence(model_list, final_modified_sentence, vocab, batch_size)
-
-    #final_modified_sentence = [k for k, v in sorted(new_sentence_dict.items(), key=lambda item: item[1])][0]
-    #new_sentence_score = new_sentence_dict[final_modified_sentence]
 
     if new_sentence_score > curr_score:
       print("new score", new_sentence_score, "curr_score", curr_score)
       print("Here is the new version of the sentence: ", final_modified_sentence)
       sentence_split = final_modified_sentence.split(" ")
-      scores.append(new_sentence_score)
       curr_score = new_sentence_score
+      changes.append((curr_score, final_modified_sentence))
+
+    scores.append(curr_score)
 
   print("New sentence is: ", " ".join(sentence_split) ," with total scores: ", scores)
 
   plot_scores(scores, ' '.join(sentence_split))
+
+  print("Full list of changes with scores", changes)
 
   return scores, ' '.join(sentence_split)
 
@@ -399,4 +405,4 @@ if __name__ == "__main__":
 
   sentence = sent_dict[sys.argv[2]]
 
-  globals()[sys.argv[1]](model_list, sentence, vocab, 100, 10)
+  globals()[sys.argv[1]](model_list, sentence, vocab, 100, 25)
