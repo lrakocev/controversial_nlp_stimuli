3a914cc0fd78a1439e4458f9901163c95fb97569
Lara Rakocevic <lrakocev@mit.edu>
2020-12-01 12:22:07 -0800

    check forsentence

---
+++ check_for_sentence.py
@@ -0,0 +1,18 @@
+import os
+import re
+
+import glob
+file_list = glob.glob('sample_judge*.out')
+
+success_id_list = []
+for file in file_list:
+    f = open(file)
+    x = f.read()
+    if "The advice would be greatly great" in x:
+        print(file)
+
+    f.close()
+
+csorted(list(set(success_id_list))))
+
+print(len(set(success_id_list)))
