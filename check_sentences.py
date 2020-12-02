aa7de05c3d279cb437e8b0c7958523d1042b1e74
Lara Rakocevic <lrakocev@mit.edu>
2020-12-01 23:00:18 -0800

    nltk

--- check_sentences.py
+++ check_sentences.py
@@ -2,23 +2,8 @@ import nltk
 nltk.download('gutenberg')
 from nltk.corpus import gutenberg
 
-def sentences_from_corpus(corpus, fileids = None):
-
-    from nltk.corpus.reader.plaintext import read_blankline_block, concat
-
-    def read_sent_block(stream):
-        sents = []
-        for para in corpus._para_block_reader(stream):
-            sents.extend([s.replace('\n', ' ') for s in corpus._sent_tokenizer.tokenize(para)])
-        return sents
-
-    return concat([corpus.CorpusView(path, read_sent_block, encoding=enc)
-                   for (path, enc, fileid)
-                   in corpus.abspaths(fileids, True, True)])
-
-
 bible=gutenberg.sents('bible-kjv.txt')
 
-sentences = sentences_from_corpus(bible)
+sentences = [" ".join(sent) for sent in bible]
 
 print(sentences)
