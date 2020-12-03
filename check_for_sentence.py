dad0cec6e41df6c001fa37ef6bad873691dc3693
Lara Rakocevic <lrakocev@mit.edu>
2020-12-02 21:44:16 -0800

    sampling logs

--- check_for_sentence.py
+++ check_for_sentence.py
@@ -5,7 +5,7 @@ import sys
 
 def check_for_sentence(sent):
 	
-	file_list = glob.glob('sampling_logs_12_01_20/sample_judge*.out')
+	file_list = glob.glob('6884_judge*.out')#sampling_logs_12_01_20/sample_judge*.out
 
 	for file in file_list:
 	    f = open(file)
