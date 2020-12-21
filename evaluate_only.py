b63c17771cbe3c42d69f084cf7fe7937e9d84458
Lara Rakocevic <lrakocev@mit.edu>
2020-12-21 12:19:40 -0800

    old school

--- evaluate_only.py
+++ evaluate_only.py
@@ -49,97 +49,26 @@ def get_vocab(filename, length):
 
   return vocab_list
 
-def get_distribution(model_name, context, vocab, n):
-
-  print("context", context)
-
-  tokenizer = model_name.tokenizer 
-  model = model_name.model.to('cuda')
-  model_word_token_dict = model_name.word_token_dict
-  model_token_id_dict = model_name.id_token_dict
-  tokenizer.pad_token = model_name.start_token_symbol
-
-  if context in model_name.distr_dict_for_context.keys():
-    print("seen it!")
-    return model_name.distr_dict_for_context[context]
-
-  context_tokens = tokenizer.tokenize(context)
-
-  final_probabilities = {}
-
-  vocab_splits = [vocab[i:i + n] for i in range(0, len(vocab), n)]
-
-  for words in vocab_splits:
-
-    batch_list = [context + str(word) for word in words]
-    
-    sub_word_token_groupings = [model_word_token_dict[word] for word in words]
-
-    lengths_contexts = [len(sub_word_tokens) for sub_word_tokens in sub_word_token_groupings]
-
-    max_length = max(lengths_contexts)
-
-    id_nums = [[model_token_id_dict[token] for token in sub_word_tokens] for sub_word_tokens in sub_word_token_groupings ]
-
-    for i in range(len(batch_list)):
-      length = lengths_contexts[i]
-      batch = batch_list[i]
-      if length < max_length: 
-        added_string = " ".join([model_name.start_token_symbol] * (max_length - length))
-        batch = batch + " " + added_string
-
-    inputs = tokenizer(batch_list, padding='longest', return_tensors="pt").to('cuda')
-
-    if model_name.model_name == "Albert":
-      attention_mask = []
-      input_ids = []
-
-      for i in range(len(batch_list)):
-        length = lengths_contexts[i]
-        tokens = tokenizer.tokenize(batch_list[i])
-        if length < max_length:
-          tokens += [tokenizer.eos_token]*(max_length-length)
-
-        ids = tokenizer.convert_tokens_to_ids(tokens)
-        input_ids.append(ids)
-        x=1
-        attention_mask.append([1 for i in range(len(tokens)-x)] + [0 for i in range(x)])
-
-      attention_mask = torch.tensor(attention_mask).to('cuda')
-      input_ids = torch.tensor(input_ids).to('cuda') 
-
-      outputs = model(input_ids, labels=input_ids, attention_mask=attention_mask)
-    else:
-      outputs = model(**inputs, labels=inputs["input_ids"])
-
-    vectorize_log = np.vectorize(math.log)
-
-    if model_name.model_name == "TXL":
-      log_probabilities = [[vectorize_log(np.asarray(outputs.prediction_scores[j][i].cpu().detach()).flatten()) for i in range(max_length-1,max_length - lengths_contexts[j],-1)] for j in range(len(batch_list))]
-
-    else:
-
-      log_probabilities = [[vectorize_log(softmax(np.asarray(outputs.logits[j][i].cpu().detach()).flatten())) for i in range(max_length-1,max_length - lengths_contexts[j],-1)] for j in range(len(batch_list))]
-
-    log_probabilities_per_tokens = [[log_probabilities[j][i][id_nums[j][i]] for i in range(len(id_nums[j])-1)] for j in range(len(batch_list))]
-
-    probabilities = [sum(log_probabilities_per_tokens[i]) for i in range(len(log_probabilities_per_tokens))]
-
-    final_probabilities.update({words[i]: probabilities[i] for i in range(len(words))})
-
-  
-  #normalizing
-  final_probabilities_total = sum(final_probabilities.values())
-  final_probabilities = {k: v / final_probabilities_total for k, v in final_probabilities.items()}
-
-  model_name.distr_dict_for_context[context] = final_probabilities
+def get_distribution(model_name, context, joint_vocab):
+  tokenizer, model = model_name.tokenizer, model_name.model
+  inputs = tokenizer(context,return_tensors='tf')
+  outputs = model(input_ids = inputs) if model_name == "t5-11b" else model(inputs)
+  ids = range(0,tokenizer.vocab_size)
+  vocab = tokenizer.convert_ids_to_tokens(ids)
+  final_vocab = set(vocab) & joint_vocab if len(joint_vocab) != 0 else set(vocab)
+  id_list = tokenizer.convert_tokens_to_ids(sorted(final_vocab))
+  outputs_array = np.asarray(outputs[0]).flatten()
+  final_outputs = [outputs_array[i] for i in id_list] 
+  probabilities = softmax(final_outputs)
+  distr_dict = dict(zip(final_vocab, probabilities))
+  return distr_dict
 
 
-  return final_probabilities
+def jsd(prob_distributions,logbase=math.e):
 
-
-def jsd(prob_distributions, weights, logbase=math.e):
-
+    n = len(prob_distributions)
+    weights = np.empty(n)
+    weights.fill(1/n)
     k = zip(weights, np.asarray(prob_distributions))
     wprobs = np.asarray([x*y for x,y in list(k)])
     mixture = wprobs.sum(axis=0)
@@ -154,38 +83,24 @@ def jsd(prob_distributions, weights, logbase=math.e):
     return(divergence)
 
 
-def evaluate_sentence(sentence, n, js_dict):
-
+def evaluate_sentence(model_list, sentence, joint_vocab):
   sentence_split = sentence.split(" ")
   len_sentence = len(sentence_split)
-
   curr_context = ""
   total_js = 0
   js_positions = []
   distrs = {}
-
   for i in range(0, len_sentence):
     curr_context += sentence_split[i] + " "
-
-    if curr_context in js_dict.keys():
-      curr_js = js_dict[curr_context]
-
-    else:
-      for model_name in model_list:
-        next_word_distr = get_distribution(model_name, curr_context, vocab, n)
-        distrs[model_name] = list(next_word_distr.values())
-
-      n = len(model_list)
-      weights = np.empty(n)
-      weights.fill(1/n)
-
-      curr_js = jsd(list(distrs.values()), weights)
-      js_dict[curr_context] = curr_js
-
+    
+    for model_name in model_list:
+      next_word_distr = get_distribution(model_name, curr_context, joint_vocab)
+      distrs[model_name] = next_word_distr
+    curr_js = jsd(distrs.values())
     total_js += curr_js
+    
     js_positions.append(curr_js)
     
-  print(total_js/len_sentence)
   return total_js/len_sentence, js_positions
 
 def sample_sentences(file_name, n):
@@ -222,5 +137,5 @@ if __name__ == "__main__":
 
   sentence = "word word word word word word word word word word" #sent_dict[sys.argv[2]]
 
-  globals()[sys.argv[1]](sentence, 100, {})
+  globals()[sys.argv[1]](model_list, sentence, {})
 
